e41646680b888ea600a2f8bf8ef8804e2d58a35c
JM <jm@disuan.co>
2019-11-17 16:24:33 +0400

    implemented staticFileServer pinipig v1.4.12

--- framework/src/routes/root.js
+++ framework/src/routes/root.js
@@ -1,27 +1,25 @@
 const pkg = require('../../package')
-const {
-  streamFile
-} = require('pinipig')
+const { staticFileServer } = require('pinipig')
 const Info = (ctx) => {
   let message = {
     result: 'Ok',
     server: 'Pinipig Server',
     app: pkg.name,
     version: pkg.version,
-    description: pkg.description
+    description: pkg.description,
   }
   ctx.res.json(message)
 }
 
-let routes = [{
+let routes = [
+  {
     url: '/info.json',
     GET: Info,
   },
   {
     url: '/*',
-    GET: streamFile('./src/public')
-  }
+    GET: staticFileServer('public'),
+  },
 ]
-
 
 module.exports = routes
